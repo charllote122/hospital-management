6ca0cb5e83de3df5cff68bdf0d7c3d1b8697523e
charllote122 <soficharllote50@gmail.com>
2026-08-14 18:56:38 +0300

    Add executable build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-echo "🚀 Building Hospital Management System..."
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
-python manage.py collectstatic --noinput --clear
-
-# Run migrations
+python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-
-echo "✅ Build complete!"
